b1e6e3c636509154c924676735a8a29370b943a7
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-10-16 11:26:39 +0200

    Bomb has SenderType

--- src/spaceinv/model/ships/Bomber.java
+++ src/spaceinv/model/ships/Bomber.java
@@ -1,18 +1,19 @@
 package spaceinv.model.ships;
 
 import spaceinv.model.projectiles.Bomb;
+import spaceinv.model.projectiles.Projectile;
 
 /*
  *   Type of space ship
  */
 public class Bomber extends AbstractSpaceShip {
-
+    private final static Projectile.Sender senderType = Projectile.Sender.INVADER;
     public Bomber(double _posX, double _posY) {
         super(_posX, _posY);
     }
 
     public Bomb createBomb() {
-        return new Bomb(getX(), getY());
+        return new Bomb(getX(), getY(), senderType);
     }
 
     @Override
