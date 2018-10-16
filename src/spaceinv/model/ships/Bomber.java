2c7a01e7cb870cd5d6789e58688da10ec7c29e00
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-10-16 10:36:42 +0200

    Bomb and Bomber ft added
    Bomb now Extends Movable and Bomber will extend Abstract SpaceShip

--- src/spaceinv/model/ships/Bomber.java
+++ src/spaceinv/model/ships/Bomber.java
@@ -5,7 +5,21 @@ import spaceinv.model.projectiles.Bomb;
 /*
  *   Type of space ship
  */
-public class Bomber {
+public class Bomber extends AbstractSpaceShip {
+//    public Bomb createBomb{
+//
+//        return new Bomb(getX(), getY());
+//    }
+
+    @Override
+    public int getPoints() {
+        return 0; //Decide points
+    }
+
+    @Override
+    public Object copyOf() {
+        return null;
+    }
 
 
 }
