7bd1eaf69a679f3a35e7010b2d1de92473a6ccb5
Epigo <DaMoller99@hotmail.se>
2018-10-16 09:06:23 +0200

    Made Gun shoot and movable

--- src/spaceinv/model/Gun.java
+++ src/spaceinv/model/Gun.java
@@ -9,9 +9,27 @@ import static spaceinv.model.SpaceInv.GAME_WIDTH;
 /*
  * A Gun for the SpaceInv game
  */
-public class Gun {
+public class Gun extends Movable {
 
     public static final double MAX_SPEED = 2;
 
+    Gun(double x, double y) {
+        super(x, y);
+    }
 
+    public Movable shoot() {
+        Movable shot = new Rocket(getSpeedX(), getSpeedY());
+        shot.setSpeed(0, Rocket.MAX_SPEED);
+        return shot;
+    }
+
+    @Override
+    public double getWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
 }
