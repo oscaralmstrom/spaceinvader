949efc6f148bf9aa8387359185261bf407883fc2
Epigo <DaMoller99@hotmail.se>
2018-10-16 09:34:11 +0200

    Added features to Rocket

--- src/spaceinv/model/projectiles/Rocket.java
+++ src/spaceinv/model/projectiles/Rocket.java
@@ -1,8 +1,24 @@
 package spaceinv.model.projectiles;
 
 
+import spaceinv.model.Movable;
+
 // Shoot by the gun
-public class Rocket {
+public class Rocket extends Movable {
 
+    public static final double MAX_SPEED = 20;
 
+    public Rocket(double x, double y) {
+        super(x, y);
+    }
+
+    @Override
+    public double getWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
 }
