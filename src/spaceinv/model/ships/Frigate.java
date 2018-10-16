a9bd68e73ffce01aa876b608b921aa5f62c1475c
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-10-16 11:14:26 +0200

    Frigate extends AbstractSS

--- src/spaceinv/model/ships/Frigate.java
+++ src/spaceinv/model/ships/Frigate.java
@@ -3,8 +3,19 @@ package spaceinv.model.ships;
 /*
  *   Type of space ship
  */
-public class Frigate  {
+public class Frigate extends AbstractSpaceShip  {
 
+    public Frigate(double _posX, double _posY) {
+        super(_posX, _posY);
+    }
 
+    @Override
+    public int getPoints() {
+        return 0; //Decide points
+    }
 
+    @Override
+    public Object copyOf() {
+        return null;
+    }
 }
