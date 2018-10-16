9b5d3aa3162ca6a4b9ce2cdf39249497ccbad557
David Möller <DaMoller99@hotmail.se>
2018-10-16 19:56:56 +0200

    Implementing Ipositionable to ground

--- src/spaceinv/model/statics/Ground.java
+++ src/spaceinv/model/statics/Ground.java
@@ -1,6 +1,8 @@
 package spaceinv.model.statics;
 
 
+import spaceinv.model.IPositionable;
+
 import static spaceinv.model.SpaceInv.GAME_HEIGHT;
 import static spaceinv.model.SpaceInv.GAME_WIDTH;
 
@@ -8,8 +10,34 @@ import static spaceinv.model.SpaceInv.GAME_WIDTH;
     Used to check if projectiles from ships have hit the ground
 
  */
-public class Ground  {
+public class Ground implements IPositionable {
 
+    private static double width = GAME_WIDTH;
+    private static double height = GAME_WIDTH / 40;
+    private static double X, Y;
 
+    public Ground() {
+        X = 0;
+        Y = GAME_HEIGHT - height;
+    }
 
+    @Override
+    public double getX() {
+        return X;
+    }
+
+    @Override
+    public double getY() {
+        return Y;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
 }
