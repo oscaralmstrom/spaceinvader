8babeeba0b8897be5d325f7c93c7bafa53cc4fbe
David Möller <DaMoller99@hotmail.se>
2018-10-16 20:02:37 +0200

    Made outer space positionable

--- src/spaceinv/model/statics/OuterSpace.java
+++ src/spaceinv/model/statics/OuterSpace.java
@@ -1,12 +1,35 @@
 package spaceinv.model.statics;
 
+import spaceinv.model.IPositionable;
+
 import static spaceinv.model.SpaceInv.GAME_WIDTH;
 
 /*
     Used to check if rockets from gun have vanished
  */
-public class OuterSpace  {
+public class OuterSpace implements IPositionable {
 
     public static final double OUTERSPACE_HEIGHT = 5;
+    public static final double width = GAME_WIDTH;
+    public static final double X = 0, Y = 0;
 
+    @Override
+    public double getX() {
+        return X;
+    }
+
+    @Override
+    public double getY() {
+        return Y;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return OUTERSPACE_HEIGHT;
+    }
 }
