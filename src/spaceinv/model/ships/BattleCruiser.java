e97ae8e5a02ecc62438c225efdea7c823bc7fffa
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-10-19 16:59:58 +0200

    BattlecCruiser init

--- src/spaceinv/model/ships/BattleCruiser.java
+++ src/spaceinv/model/ships/BattleCruiser.java
@@ -1,10 +1,37 @@
 package spaceinv.model.ships;
 
+import spaceinv.model.projectiles.Projectile;
+
 /*
  *   Type of space ship
  */
-public class BattleCruiser {
+public class BattleCruiser extends AbstractSpaceShip{
+    public BattleCruiser(double _posX, double _posY) {
+        super(_posX, _posY);
+    }
 
-    public BattleCruiser() {
+    @Override
+    public int getPoints() {
+        return 0;
+    }
+
+    @Override
+    public Projectile fire() {
+        return null;
+    }
+
+    @Override
+    public Object copyOf() {
+        return null;
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
     }
 }
